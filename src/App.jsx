e39ac494106e40cd8d818e622264c05dc1d442ec
AndriusDulevicius <andriusdulevicius@email.com>
2021-06-30 15:29:55 +0300

    general cleanup, starting to create components

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import SearchBar from './components/searchBar/searchBar';
+import Articles from './components/articles/articles';
+
+function App() {
+  return (
+    <div className='App'>
+      <SearchBar />
+      <Articles />
+    </div>
+  );
+}
+
+export default App;
